40dc4b4ce0166ce193a5a211a9013ff19a1e930b
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-22 19:04:30 +0500

    Task 2

--- app/src/main/java/com/madlab/journal3/MainActivity.kt
+++ app/src/main/java/com/madlab/journal3/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.madlab.journal3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputOne: EditText
+    private lateinit var inputTwo: EditText
+    private lateinit var result: TextView
+    private lateinit var buttonAdd: Button
+    private lateinit var buttonSubtract: Button
+    private lateinit var buttonMultiply: Button
+    private lateinit var buttonDivide: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputOne = findViewById(R.id.numberOne)
+        inputTwo = findViewById(R.id.numberTwo)
+        result = findViewById(R.id.result)
+        buttonAdd = findViewById(R.id.addButton)
+        buttonSubtract = findViewById(R.id.subtractButton)
+        buttonMultiply = findViewById(R.id.multiplyButton)
+        buttonDivide = findViewById(R.id.divideButton)
+
+        result.text = getString(R.string.result_default)
+
+        buttonAdd.setOnClickListener {
+            val numberOne = inputOne.text.toString().toFloatOrNull()
+            val numberTwo = inputTwo.text.toString().toFloatOrNull()
+            if (numberOne != null && numberTwo != null) {
+                result.text = (numberOne + numberTwo).toString()
+            } else {
+                result.text = getString(R.string.result_error)
+            }
+        }
+        buttonSubtract.setOnClickListener {
+            val numberOne = inputOne.text.toString().toFloatOrNull()
+            val numberTwo = inputTwo.text.toString().toFloatOrNull()
+            if (numberOne != null && numberTwo != null) {
+                result.text = (numberOne - numberTwo).toString()
+            } else {
+                result.text = getString(R.string.result_error)
+            }
+        }
+        buttonMultiply.setOnClickListener {
+            val numberOne = inputOne.text.toString().toFloatOrNull()
+            val numberTwo = inputTwo.text.toString().toFloatOrNull()
+            if (numberOne != null && numberTwo != null) {
+                result.text = (numberOne * numberTwo).toString()
+            } else {
+                result.text = getString(R.string.result_error)
+            }
+        }
+        buttonDivide.setOnClickListener {
+            val numberOne = inputOne.text.toString().toFloatOrNull()
+            val numberTwo = inputTwo.text.toString().toFloatOrNull()
+            if (numberOne != null && numberTwo != null) {
+                result.text = (numberOne / numberTwo).toString()
+            } else {
+                result.text = getString(R.string.result_error)
+            }
+        }
     }
 }
