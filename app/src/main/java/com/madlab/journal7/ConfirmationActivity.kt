e29208663305b90b23a9c1ff28b7de791cca2049
MZO9400 <hamza.hameed00@yahoo.com>
2021-12-16 17:29:06 +0500

    Create confirmation activity

---
+++ app/src/main/java/com/madlab/journal7/ConfirmationActivity.kt
@@ -0,0 +1,11 @@
+package com.madlab.journal7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+    }
+}
